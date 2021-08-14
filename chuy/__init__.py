3a0e83de2684dba32df0ed26216435a129bfc1f9
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-08-13 22:21:52 -0500

    :bug: Fix a bug in Actions

--- chuy/__init__.py
+++ chuy/__init__.py
@@ -1,1 +1,11 @@
-__version__ = "0.1.0"
+"""
+This file define chuy as a module.
+"""
+
+from .core import exec_commands, get_config
+
+__version__ = "0.1.1"
+__authors__ = ["Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>"]
+__author_email__ = "eliaz.bobadilladev@gmail.com"
+__url__ = "https://github.com/UltiRequiem/chuy"
+__package_name__ = "chuy"
