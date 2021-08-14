d6ba89f19fea1023f9bfbd798ce84a0792c176b1
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-08-14 11:58:28 -0500

    :bookmark: v0.1.6

--- chuy/core.py
+++ chuy/core.py
@@ -2,8 +2,24 @@ import json
 import os
 import sys
 
-# TODO: Print colorized output
-from colorama import Fore, Back, Style
+from colorama import Fore, Style, init
+
+if sys.platform == "win32":
+    init()
+
+
+def manage_keyboard_interrumpt(func):
+    def manager():
+        try:
+            func()
+        except KeyboardInterrupt:
+            print(f"\n {Fore.BLUE}Goodbye!")
+            try:
+                sys.exit(0)
+            except SystemExit:
+                os._exit(0)
+
+    return manager
 
 
 def get_config(configuration_file: str) -> dict:
@@ -14,6 +30,12 @@ def get_config(configuration_file: str) -> dict:
         raise BaseException("Your configuration is invalid!") from invalid_config
     except FileNotFoundError as file_not_found:
         raise BaseException("File does not exist!") from file_not_found
+
+
+def print_with_color(text, color=Fore.WHITE, brightness=Style.NORMAL, **kwargs):
+    """Utility function wrapping the regular `print()` function
+    but with colors and brightness"""
+    print(f"{brightness}{color}{text}{Style.RESET_ALL}", **kwargs)
 
 
 # TODO: Validate the schema
@@ -28,22 +50,24 @@ def exec_commands(command):
 
 
 def list_commands(config: dict):
-    print("Proyect Commands:")
+    print_with_color(" Proyect Commands:", Fore.CYAN)
     for item in config:
-        print(
+        print_with_color(
             f"""
   - {item}
       $ {config[item]}
-        """
+        """,
+            color=Fore.RED,
         )
 
-    command = input("Which command do you want to run? ").lower()
+    command = input(f" {Fore.YELLOW}Which command do you want to run? ").lower()
     try:
         exec_commands(config[command])
     except KeyError as command_not_exist:
         raise BaseException("That is not a valid command!") from command_not_exist
 
 
+@manage_keyboard_interrumpt
 def main():
     config = get_config("chuy.json")
 
