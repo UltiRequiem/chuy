684c044a16d177cb6d9fad5f539d1c5aae0147d4
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-08-13 22:47:37 -0500

    :white_check_mark: Add more test cases

--- tests/test_chuy.py
+++ tests/test_chuy.py
@@ -1,5 +1,17 @@
-from chuy import __version__
+from chuy import __package_name__, __url__, __version__, __author_email__
 
 
 def test_version():
-    assert __version__ == "0.1.0"
+    assert isinstance(__version__, str), "The version value is invalid!"
+
+
+def test_url():
+    assert isinstance(__url__, str), "The url value is invalid!"
+
+
+def test_name():
+    assert isinstance(__package_name__, str), "The package name value is invalid!"
+
+
+def test_email():
+    assert isinstance(__author_email__, str), "The email value is invalid!"
